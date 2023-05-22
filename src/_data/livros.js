f30867af2cdda2139bc122719c9fe96e8ed81c65
VictorSedlak1 <victorsedlak04@gmail.com>
2023-05-22 11:58:35 -0300

    alt3

---
+++ src/_data/livros.js
@@ -0,0 +1,30 @@
+export const livros = [
+    {
+      id: 1,
+      title: "O poderoso chefão",
+      author: "Nigga",
+      price: 49.9,
+      cover: "https://i.ytimg.com/vi/cjOpDYTVgi8/maxresdefault.jpg",
+    },
+    {
+      id: 1,
+      title: "O poderoso chefão",
+      author: "Nigga",
+      price: 49.9,
+      cover: "https://i.ytimg.com/vi/cjOpDYTVgi8/maxresdefault.jpg",
+    },
+    {
+      id: 1,
+      title: "O poderoso chefão",
+      author: "Nigga",
+      price: 49.9,
+      cover: "https://i.ytimg.com/vi/cjOpDYTVgi8/maxresdefault.jpg",
+    },
+    {
+      id: 1,
+      title: "O poderoso chefão",
+      author: "Nigga",
+      price: 49.9,
+      cover: "https://i.ytimg.com/vi/cjOpDYTVgi8/maxresdefault.jpg",
+    },
+  ]
